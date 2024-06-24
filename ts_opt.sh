5e3bb28f93d094a7d9f6d613ec906c691294973b
Bastian Schnieder <101337667+bastianschnieder@users.noreply.github.com>
2024-06-24 10:31:14 +0200

    Add files via upload

---
+++ ts_opt.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=20
+#SBATCH --error=err-%j.log
+#SBATCH --output=out-%j.log
+#SBATCH --time=300-22:00:00
+
+ 
+HDIR=$(pwd)
+
+export WDIR=$TMPDIR/$SLURM_JOB_NAME
+
+srun mkdir -p $WDIR
+
+#cpc $WDIR
+cp $HDIR/* $WDIR
+cd $WDIR
+
+
+export TMP=$TMPDIR/$SLURM_JOBID
+# do not remove /$SLURM_JOBID
+ 
+srun mkdir -p $TMP
+# -p option make the $TMPDIR directory on all nodes
+  
+#setup turbomole:
+export PARA_ARCH=SMP
+export PARNODES=20
+export TURBODIR=/opt/software/user-soft/turbomole/76
+export PATH=$TURBODIR/bin/`sysname`:$PATH
+export PATH=$TURBODIR/scripts:$PATH
+export TURBOTMPDIR=$TMP
+  
+# TURBOMOLE command(s):
+jobex -ri -trans -gcart 4 -c 900 > jobex.out
+ 
+rm -rf auxboi auxbai ftraux fock oldfock errvec
+rm -rf dens ddens mdens core twoint*
+rm -rf CC_* CC1DEN* CCJCOUINTE CCTR* CCVPQ_ISQR CCB* CCG* CCEQB* CCY* CCI* CCFOCK_LMO
+rm -rf RIR12_*
+rm -rf half* gamma* moint* alles.cc syminfo oneint
+rm -rf statistics.*
+
+rm -rf $TMP*
+
+mkdir $HDIR/BACK
+cp -r $WDIR/* $HDIR/BACK/.
